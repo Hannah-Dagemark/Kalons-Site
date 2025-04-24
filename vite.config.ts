cdeaffe8a992513add182ba1cde2869b5e73dbee
hannah.dagemark <hannah.dagemark@gmail.com>
2025-04-24 15:11:47 +0200

    Update settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    port: 5000,
+  },
   plugins: [react(), tailwindcss()],
 })
